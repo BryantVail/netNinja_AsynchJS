e504c114a576159b934a80949ad3f3a089cc2fec
bxvail <BryantVail@gmail.com>
2018-03-18 21:30:46 -0400

    examples.js

---
+++ examples.js
@@ -0,0 +1,19 @@
+//Synchronous = javascript can do 1 thing at a time
+
+//let article = ReadSync(article_loc);
+console.log("article");
+
+//let authors = ReadSync(authors_loc);
+console.log("authors");
+
+//Asynchronous = send job, next, 
+
+/*
+readAsync(article_loc, function(){
+    console.log(article)
+});
+
+readAsync(authors_loc, function(){
+    console.log(authors);
+});
+*/
